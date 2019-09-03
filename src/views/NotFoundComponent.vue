b435d263b6434e175f91f3c9d88fd92debeaf57e
Pedro Oberto <pedroobertol5@gmail.com>
2019-09-02 21:26:41 -0300

    Pagina Not Found

--- src/views/NotFoundComponent.vue
+++ src/views/NotFoundComponent.vue
@@ -1,13 +1,23 @@
 <template>
   <section>
+    <img src="@/assets/notfound.png" alt="Pagina Não Encontrada" />
     <h1>Pagina Não Encontrada 404</h1>
     <router-link to="/" class="button">Transport</router-link>
   </section>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "NotFoundComponent",
+  created() {
+    document.title = "Pagina Não Encontrada 404";
+  }
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+img {
+  width: 250px;
+  margin: 100px auto 0;
+}
 </style>
